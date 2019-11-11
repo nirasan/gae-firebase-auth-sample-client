e6d576f067c26009d77cd0045103bd80432f0ff5
nirasan <niranirao@gmail.com>
2019-11-11 19:18:06 +0900

    authorization header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
     <div *ngIf="afAuth.user | async as user; else showLogin">
       <h1>Hello {{ user.displayName }}!</h1>
       <button (click)="logout()">Logout</button>
-      <button (click)="idToken()">IDToken</button>
+      <button (click)="accessServer()">Access Server</button>
     </div>
     <ng-template #showLogin>
       <p>Please login.</p>
@@ -20,16 +20,19 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   constructor(public afAuth: AngularFireAuth, private http: HttpClient) {
   }
+  // Firebase Authentication のログイン実行
   login() {
     this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
   }
+  // Firebase Authentication のログアウト実行
   logout() {
     this.afAuth.auth.signOut();
   }
-  idToken() {
+  // Firebase Authentication の ID Token をヘッダに付与してサーバーアクセスする
+  accessServer() {
     this.afAuth.auth.currentUser.getIdToken().then((idToken: string) => {
-      console.log(idToken);
-      this.http.get('http://localhost:8080', {params: {token: idToken}}).subscribe((res: string) => {
+      const header = new HttpHeaders().set('Authorization', 'Bearer ' + idToken);
+      this.http.get('http://localhost:8080', {headers: header}).subscribe((res: string) => {
         console.log(res);
       });
     });
